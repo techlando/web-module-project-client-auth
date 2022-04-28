a9c9b8ae95e548894034607bace487e6b4bdab5d
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-04-28 10:42:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+const Login = () => {
+  return (<h2>Login</h2>)
+}
+const FriendsList = () => {
+  return (<h2>FriendsList</h2>)
+}
+const AddFreind = () => {
+  return (<h2>AddFreind</h2>)
+}
+ 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Route  exact path="/">
+        <Login/>
+      </Route>
+      <Route exact path="/login">
+        <Redirect to="/"/>
+      </Route>
+      <Route exact path="/friends">
+        <FriendsList/>
+      </Route>
+      <Route exact path="/friends/add">
+        <AddFreind/>
+      </Route>
     </div>
+    </Router>
   );
 }
 
 export default App;
+ 
